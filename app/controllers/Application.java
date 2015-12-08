43ac83d8c11a7713c13a07aaedb785d9b9ca74da
ann <jeongchanyang@gmail.com>
2015-12-08 16:47:47 +0900

    using view tags

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,6 +8,13 @@ import views.html.*;
 
 public class Application extends Controller {
 
+    private final java.util.Map<Integer,String> productMap = new java.util.HashMap<Integer,String>();
+
+    public Application() {
+        productMap.put(1,"Keyboard");
+        productMap.put(2,"Mouse");
+        productMap.put(3,"Monitor");
+    }
     public Result index() {
         return ok(index.render("Your new application is ready."));
     }
@@ -18,5 +25,8 @@ public class Application extends Controller {
 
     public Result helloRedirect() {
         return redirect(controllers.routes.Application.echo("redirect"));
+    }
+    public Result listProducts() {
+        return ok(products.render(productMap.values()));
     }
 }
